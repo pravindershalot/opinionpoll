80999cf1693bc6924c3412551c8fbc73c81e80c2
pravindershalot <84888080+pravindershalot@users.noreply.github.com>
2021-05-27 08:48:36 +0530

    Create result.html.php

---
+++ result.html.php
@@ -0,0 +1,25 @@
+<html>
+
+    <head>
+
+        <title>Poll Results</title>
+
+    </head>
+
+    <body>
+
+        <h2>Opinion Poll Results</h2>
+
+        <p><b>Who is your favorite Author?</b></p>
+
+        <p><b><?php echo $choices_count[0]; ?></b> people have thus far taken part in this poll:</p>
+
+        <p>
+<table>
+
+            <?php echo($table_rows); ?>
+
+        </table>
+</body>
+
+</html>
